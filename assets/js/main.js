2523d968cf9e8ad2269c8a703567a138042e791b
bedimcode <61248995+bedimcode@users.noreply.github.com>
2021-08-27 11:41:25 -0500

    Add files via upload
    Adding files to the project

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+/*=============== ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.accordion__item')
+
+// 1. Selecionar cada item
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.accordion__header')
+
+    // 2. Seleccionar cada click del header
+    accordionHeader.addEventListener('click', () =>{
+        // 7. Crear la variable
+        const openItem = document.querySelector('.accordion-open')
+        
+        // 5. Llamar a la funcion toggle item
+        toggleItem(item)
+
+        // 8. Validar si existe la clase
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+// 3. Crear una funcion tipo constante
+const toggleItem = (item) =>{
+    // 3.1 Crear la variable
+    const accordionContent = item.querySelector('.accordion__content')
+
+    // 6. Si existe otro elemento que contenga la clase accorion-open que remueva su clase
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        // 4. Agregar el height maximo del content
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
